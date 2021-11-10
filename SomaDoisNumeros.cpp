c60ad609cf71e8081dc958bb9ba107abdde80201
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2021-11-10 19:14:31 -0300

    Create SomaDoisNumeros.cpp

---
+++ SomaDoisNumeros.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+int main ()
+{
+  int numero1=, numero2=, soma=;
+  
+  count << "PROGRAMA PARA CALCULO DA SOMA DE DOIS NUMEROS INTEIROS" << endl;
+  cont << "Digite o valor do primeiro numero inteiro e tecle enter: " <<endl;
+  cin >> numero1;
+  count << "Digite o valor do segundo numero inteio e tecle enter: " << endl;
+  cin >> numero2;
+  
+  soma = numeor1 + numero2;
+  
+  count << "Soma = " << soma << endl;
+  
+  sustem("PAUSE");
+  
+  return 0;
+}
