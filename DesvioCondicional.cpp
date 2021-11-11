7833ad24adc56535401e8446f87740276a3eb479
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2021-11-11 09:53:28 -0300

    Create DesvioCondicional.cpp

---
+++ DesvioCondicional.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+  int numero1=0, numero2=0, soma=0;
+  
+  cout << "PROGRAMA PARA VERIFICAR SE A SOMA DE DOIS NUMEROS E MAIOR QUE 100" << endl << endl;
+  cout << "Digite o primeiro numero e tecle enter: ";
+  cin >> numero1;
+  cout << "digite o segundo numero e tecle enter: ";
+  cin >> numero2;
+  count << endl;
+  
+  soma = numeri1 + numero2;
+  
+  if (soma > 100)
+  {
+    cout << "SOMA MAIOR QUE 100" << endl;
+  }
+  cout << "Soma =" << soma << endl;
+  
+  system("PAUSE");
+  
+  return 0;
+}  
