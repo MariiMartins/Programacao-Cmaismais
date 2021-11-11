4e701391058956c4e2d7d9e8b67c516fd4f04698
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2021-11-11 10:13:53 -0300

    Create CalculoMedia.cpp

---
+++ CalculoMedia.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+  float nota1=0, nota2=0, media=0;
+
+  cout << "PROGRAMA PARA CALCULAR A MEDIA DE UM ALUNO" <<endl;
+  cout << "digite o valor da primeira nota e tecle enter: ";
+  cin >> nota1;
+  cout << "Digite o valor da segunda nota e tecle enter: ";
+  cin >> nota2;
+  count << endl;
+
+  media = (nota1 + nota2) / 2.0;
+
+  if (media >= 7.0)
+  {
+    cout << "Media = " << media << "ALUNO APROVADO" << endl;
+  }
+  else
+  {
+    cout << "Media = " << meida << "ALUNO REPROVADO" << endl;
+  }  
+  system("PAUSE");
+  return 0;
+}  
