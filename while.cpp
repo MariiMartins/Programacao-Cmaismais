1c3dbcc387dc674ad688431749f651bb0fc8fc78
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2021-11-17 18:54:42 -0300

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -4,20 +4,17 @@
 using namespace std;
 
 int main() {
-	  
-  int numero=0, contador=0, soma=0;
+	int numero = 0, contador = 0, soma = 0;
 
-  while (numero != -1)
-  {
-    cout << "Digite um número inteiro positivo. Para encerrar digite -1" << endl;
-    cin >> numero;
-    if (numero != -1)
-    {
-      soma = soma + numero;
-    }
-
-  }
-	cout << "Foram digitados " << contador <<< "numeros" << endl;
-  cout << "A soma dos numeros digitados igual a " << soma << endl;
-  return 0;    
-}  
+	while (numero != -1) {
+		cout << "Digite um número inteiro positivo. Para encerrar digite -1"
+			 << endl;
+		cin >> numero;
+		if (numero != -1) {
+			soma = soma + numero;contador = contador +1;
+		}
+	}
+	cout << "Foram digitados " << contador << " numeros" << endl;
+	cout << "A soma dos numeros digitados igual a " << soma << endl;
+	return 0;
+}
