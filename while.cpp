6e999acc5463c42040dc1b2377eb2947c75d977e
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2021-11-17 18:26:38 -0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	  
+  int numero=0, contador=0, soma=0;
+
+  while (numero != -1)
+  {
+    cout << "Digite um número inteiro positivo. Para encerrar digite -1" << endl;
+    cin >> numero;
+    if (numero != -1)
+    {
+      soma = soma + numero;
+    }
+
+  }
+	cout << "Foram digitados " << contador <<< "numeros" << endl;
+  cout << "A soma dos numeros digitados igual a " << soma << endl;
+  return 0;    
+}  
